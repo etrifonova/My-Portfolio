76dedfa4de4bf90ccb3ba2e9bfdfe420846a628e
etrifonova <101653327+lenator@users.noreply.github.com>
2023-01-12 17:14:30 +0300

    feat: update index & styles and add app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+
+function pageTransitions() {
+    //Buttons click active class
+    for(let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener('click', function() {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn'
+        })
+    }
+}
+// 'this' keyword doesn't exist in arrow functions, which is why there was an error in the arrow function; it was then changed for a usual function
+pageTransitions()
